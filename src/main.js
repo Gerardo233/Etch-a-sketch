141a1ac5d8a774d444f05391fd148829d504bf15
Gerardo233 <miguelmr1107@gmail.com>
2024-08-03 04:37:48 -0600

    feat:boom: Se termino la funcionalidad del grid

--- src/main.js
+++ src/main.js
@@ -1,64 +1,91 @@
-let controllers = document.querySelector(".controllers");
-let gridContainer = document.querySelector(".grid-content");
-let change = new CustomEvent("change");
+let controllers = document.querySelector('.controllers');
+let gridContainer = document.querySelector('.grid-content');
 
-let prueba = document.getElementById("grid");
+let gridSize = document.querySelector('#grid');
+
+let myEvent = new CustomEvent('change');
+
+//Controllers inputs variables
+const gridInput = document.querySelector('#grid');
+const colorInput = document.querySelector('#color');
+
+// Set default values
+const defaultGridSize = 16;
+const defaultColor = '#000000';
 
 /*Equivalencias de medidas*/
 let containerX = gridContainer.clientWidth;
 let containerY = gridContainer.clientHeight;
 
-let selectedColor = "";
-window.addEventListener("load", function (e) {
-  controllers.dispatchEvent(change);
+let selectedColor = '';
+
+//Verificar
+var isClicked = false;
+
+window.addEventListener('load', function (e) {
+  gridSize.value = defaultGridSize;
+  colorInput.value = defaultColor;
+  selectedColor = defaultColor;
+
+  // Trigger the change event to initialize the grid
+  createSquares(defaultGridSize, gridContainer);
 });
 
-controllers.addEventListener("change", function (e) {
+controllers.addEventListener('change', function (e) {
   let color;
   switch (e.target.id) {
-    case "grid":
+    case 'grid':
       createSquares(e.target.value, gridContainer);
-      console.log(e.target.value);
-
       break;
 
-    case "color":
+    case 'color':
       color = e.target.value;
       selectedColor = catchColor(color);
       break;
   }
 });
 
-gridContainer.addEventListener("mouseover", function (e) {
-  if (
-    e.target.classList.contains("square") ||
-    e.target.classList.contains("childSquare")
-  ) {
+gridContainer.addEventListener('mouseover', function (e) {
+  if (isClicked) {
+    if (
+      e.target.classList.contains('square') ||
+      e.target.classList.contains('childSquare')
+    ) {
+      e.target.style.backgroundColor = selectedColor;
+    }
+  }
+});
+
+gridContainer.addEventListener('click', function (e) {
+  if (isClicked === true) {
+    isClicked = false;
+  } else if (isClicked === false) {
+    isClicked = true;
     e.target.style.backgroundColor = selectedColor;
   }
 });
 
 function createSquares(range, parent) {
-  parent.innerHTML = ""; // Equivalent to jQuery's empty()
+  parent.innerHTML = ''; // Equivalent to jQuery's empty()
   createRows(parent, range);
   placeSquares(parent, range);
 }
 
 function createRows(container, range) {
   for (let i = 0; i < range; i++) {
-    const divSquare = document.createElement("div");
+    const divSquare = document.createElement('div');
     divSquare.style.height = `${containerY / range}px`;
-    divSquare.classList.add("square");
+    divSquare.classList.add('square');
     container.appendChild(divSquare);
   }
 }
 
 function createColumns(container, range) {
   for (let i = 0; i < range; i++) {
-    const divSquare = document.createElement("div");
+    const divSquare = document.createElement('div');
     divSquare.style.height = `${containerY / range}px`;
     divSquare.style.width = `${containerX / range}px`;
-    divSquare.classList.add("childSquare");
+    divSquare.classList.add('childSquare');
     container.appendChild(divSquare);
   }
 }
